80399693c245d0d13b58174ff08e1975ae489d68
krutarth <74930393+krutarth4@users.noreply.github.com>
2024-06-22 15:54:19 +0200

    #13 app routing init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+// @ts-ignore
+export const routes: Routes = [
+  {path: '',redirectTo:'/home', pathMatch: 'full'},
+  {
+    path:'home',
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),
+  },
+];
