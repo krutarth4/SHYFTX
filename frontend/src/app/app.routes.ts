4bc15b10cd8b074036ddb73b26bb337792be1ef6
krutarth <74930393+krutarth4@users.noreply.github.com>
2024-06-30 14:09:16 +0200

    redirect changed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {HomeComponent} from "./components/home/home.component";
 
 // @ts-ignore
 export const routes: Routes = [
-  {path: '',redirectTo:'/farmer', pathMatch: 'full'},
+  {path: '',redirectTo:'/home', pathMatch: 'full'},
   {
     path:'home',
     loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),
