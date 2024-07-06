a95185d38334e7898ce59857ccf2bdcc521969c6
krutarth <74930393+krutarth4@users.noreply.github.com>
2024-07-06 14:31:41 +0200

    #58 authFirebase integrated

--- frontend/src/app/services/authFirebase/auth-firebase.service.ts
+++ frontend/src/app/services/authFirebase/auth-firebase.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthFirebaseService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+
+  private signUp(email: string, password: string) {
+    this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        // Sign up successful
+      })
+      .catch((error) => {
+        // An error occurred
+      });
+  }
+
+  private login(email: string, password: string) {
+    this.afAuth.signInWithEmailAndPassword(email, password)
+      .then(() => {
+        // Login successful
+      })
+      .catch((error) => {
+        // An error occurred
+      });
+  }
+
+  private logout() {
+    this.afAuth.signOut()
+      .then(() => {
+        // Logout successful
+      })
+      .catch((error) => {
+        // An error occurred
+      });
+  }
+  //getter method as a property
+
+  get isAuthenticated(): boolean {
+    return this.afAuth.currentUser !== null;
+  }
+
+
+
+
 }
